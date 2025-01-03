0b20978f9bb00b1cbb18cd134bbb12b38a97648e
Muoi Hay Code Dao <lonelyguitarist2745@gmail.com>
2025-01-03 16:58:25 +0700

    hoan thanh giao dien header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -10,11 +10,12 @@ const Header = () => (
           <span className={styles.appName}>NALOPOMO</span>
         </a>
       </div>
-      <ul className={styles.menu}>
-        <li><a href="/"><i className="fa-solid fa-chart-simple"></i> Report</a></li>
-        <li><a href="/about"><i className="fa-solid fa-gear"></i> Settings</a></li>
-        <li><a href="/contact">Sign In</a></li>
-      </ul>
+      <span className={styles.menu}>
+        <button><a href="/"><i className="fa-solid fa-chart-simple"></i> Report</a></button>
+        <button><a href="/about"><i className="fa-solid fa-gear"></i> Settings</a></button>
+        <button><a href="/contact"><i className="fa-solid fa-user"></i> Sign In</a></button>
+        <button><a href="#"><i className="fa-solid fa-bars"></i></a></button>
+      </span>
     </nav>
   </header>
 )
